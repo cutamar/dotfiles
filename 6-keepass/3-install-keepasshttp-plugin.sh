e75d40ea2b256ddf8ee0e395e06458aea646be32
cutamar <amar.cutura@gmail.com>
2017-11-07 14:54:23 +0100

    fixed keepass config

--- 6-keepass/3-install-keepasshttp-plugin.sh
+++ 6-keepass/3-install-keepasshttp-plugin.sh
@@ -1,2 +1,3 @@
 sudo mkdir -p /usr/share/keepass/Plugins
-sudo wget https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx -o /usr/share/keepass/Plugins/KeePassHttp.plgx
+wget -nc https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx
+sudo cp -f KeePassHttp.plgx /usr/share/keepass/Plugins/
--- 6-keepass/4-cp-keepass-config.sh
+++ 6-keepass/4-cp-keepass-config.sh
@@ -1,2 +1,2 @@
-mkdir -p ~/.config/Keepass
+mkdir -p ~/.config/KeePass
 cp -f config/KeePass.config.xml ~/.config/KeePass/KeePass.config.xml
