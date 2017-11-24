7d99718d95a02aebe911bd79661e51fd23cf874e
cutamar <amar.cutura@gmail.com>
2017-11-24 01:37:50 +0100

    firefox quantum enable built-in u2f

--- 5-firefox/4-install-u2f-addon.sh
+++
@@ -1,2 +0,0 @@
-wget https://addons.mozilla.org/firefox/downloads/file/557965/u2f_support_add_on-1.0.1-fx-linux.xpi
-firefox u2f_support_add_on-1.0.1-fx-linux.xpi
--- 5-firefox/config/user.js
+++ 5-firefox/config/user.js
@@ -2,3 +2,5 @@
 user_pref("browser.startup.page", 3);
 // do not remember logins
 user_pref("signon.rememberSignons", false);
+// u2f
+user_pref("security.webauth.u2f", true);
