5730b977a9929a1c99e6e9482381b2055dd7a996
cutamar <amar.cutura@gmail.com>
2018-03-27 20:52:31 +0200

    xfce additions

---
+++ 1-init/15-install-window-move.sh
@@ -0,0 +1,2 @@
+sudo pacman -S xdotool wmctrl xorg-xprop xorg-xwininfo xorg-xdpyinfo --noconfirm
+ln -sf $PWD/config/scripts ~/.scripts
---
+++ 1-init/16-ln-xfce-config.sh
@@ -0,0 +1,2 @@
+rm -rf ~/.config/xfce4 
+ln -sf $PWD/config/xfce4 ~/.config/xfce4
