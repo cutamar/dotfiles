54d45652a8d7833b6b018a0a7e795825ae7b9074
cutamar <amar.cutura@gmail.com>
2018-04-20 01:31:51 +0200

    changed chicago theme installer

--- 8-themes/1-install-chicago95.sh
+++ 8-themes/1-install-chicago95.sh
@@ -2,7 +2,7 @@ git clone https://github.com/grassmunk/Chicago95.git
 cd Chicago95
 mkdir -p ~/.local/share/themes
 cp -rf Theme/Chicago95 ~/.local/share/themes/
-cp -rf Icons/Chicago95 ~/.icons/
+cp -rf Icons/Chicago95 ~/.icons/Chicago95
 cp -rf Cursors/Chicago95_Cursor_Black/cursors ~/.icons/Chicago95/
 mkdir -p ~/.fonts/truetype
 cp -rf Fonts/vga_font ~/.fonts/truetype/
